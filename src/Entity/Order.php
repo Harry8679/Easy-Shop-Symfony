a0d5e6bc2f0388782e2b086af2d1e136610e77bf
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-09-13 09:53:03 +0200

    Order Entity

---
+++ src/Entity/Order.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderRepository::class)]
+#[ORM\Table(name: '`order`')]
+class Order
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $userName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $userEmail = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $userAddress = null;
+
+    #[ORM\Column]
+    private ?float $total = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(string $userName): static
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    public function getUserEmail(): ?string
+    {
+        return $this->userEmail;
+    }
+
+    public function setUserEmail(string $userEmail): static
+    {
+        $this->userEmail = $userEmail;
+
+        return $this;
+    }
+
+    public function getUserAddress(): ?string
+    {
+        return $this->userAddress;
+    }
+
+    public function setUserAddress(string $userAddress): static
+    {
+        $this->userAddress = $userAddress;
+
+        return $this;
+    }
+
+    public function getTotal(): ?float
+    {
+        return $this->total;
+    }
+
+    public function setTotal(float $total): static
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+}
