1ddbc75889a40d9f756e3321cf683b2811a4412d
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-09-15 04:18:19 +0200

    List of Products 3

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ProductController extends AbstractController
 {
     #[Route('/admin/products', name: 'app_product')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
-        return $this->render('admin/product/index.html.twig');
+        $products = $productRepository->findAll();
+
+        return $this->render('admin/product/index.html.twig', [
+            'products' => $products
+        ]);
     }
 }
