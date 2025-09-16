602f56930d48fd2eb4c8a0b9e3cfca3b4387a63f
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-09-16 10:35:33 +0200

    New Action: add product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +20,22 @@ final class ProductController extends AbstractController
 
         return $this->render('admin/product/index.html.twig', [
             'products' => $products
+        ]);
+    }
+
+    public function new(Request $request, EntityManagerInterface $em)
+    {
+        $product = new Product();
+
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $product->setCreatedAt(new \DateTimeImmutable());
+        }
+
+        return $this->render('admin/product/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
