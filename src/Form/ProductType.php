86202830e4f46ed4f59e6d7e1652af1de581c7a8
Rudolph Harry EBANG <emarh.harry.code@gmail.com>
2025-09-16 10:13:47 +0200

    Product Form 2

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -6,18 +6,36 @@ use App\Entity\Category;
 use App\Entity\Product;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File as ConstraintsFile;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, ['label' => 'Name'])
             ->add('description')
             ->add('price')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'class' => 'w-full p-2 border rounded-lg text-black bg-white',
+                    'accept' => 'image/*'
+                ],
+                'label_attr' => ['class' => 'text-gray-700 font-semibold'],
+                'constraints' => [
+                    new ConstraintsFile([
+                        'maxSize' => '2M',
+                        'mimeTypes' => ['image/jpeg', 'image/png', 'image/jpg', 'image/gif', 'image/webp'],
+                        'mimeTypesMessage' => 'Please upload a valid image (JPEG, PNG, JPG, GIF, WEBP)'
+                    ])
+                ]
+            ])
             ->add('createdAt', null, [
                 'widget' => 'single_text',
             ])
